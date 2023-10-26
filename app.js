9257c8170ee1e70c3e257a3fbd42ade90d4361ac
Kim Weast <weastkim3@gmail.com>
2023-10-26 06:55:02 -0500

    update

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-/**
- *
- * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
- */
-function getList() {
+// Exercise 1
+const errorParagraph = document.getElementById("error");
+const listUl = document.getElementById("list");
+
+// Exercise 2
+const getList = () => {
   return new Promise((resolve, reject) => {
+    // Simulating an asynchronous operation
     setTimeout(() => {
-      let potentialFail = Math.round(Math.random() * 100) < 10;
-      if (potentialFail) {
-        reject({ success: false, message: "Failed to get list of hobbits." });
+      const success = true;
+      if (success) {
+        const hobbits = ["Frodo Baggins", "Samwise Gamgee", "Merry Brandybuck", "Peregrin Took"];
+        resolve(hobbits);
       } else {
-        resolve(["Bilbo", "Frodo", "Sam", "Merry", "Pippin"]);
+        const failure = {
+          message: "Error: The list could not be retrieved."
+        };
+        reject(failure);
       }
-    }, 10);
+    }, 2000);
   });
-}
+};
 
-// TODO: Handle the resolved or rejected states of the promise
+// Exercise 2: Handling the Promise
+getList()
+  .then((result) => {
+    console.log(result); // Logging the resolved value
+    // Exercise 3: Update the DOM
+    result.forEach((hobbit) => {
+      const li = document.createElement("li");
+      li.textContent = hobbit;
+      listUl.appendChild(li);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message); // Logging the error message
+    // Exercise 3: Update the DOM
+    errorParagraph.textContent = error.message;
+  });
 
-// TODO: If the promise resolves with the list of hobbits
-// Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
-// TODO: If the promise rejects with the failure object
-// Display the failure message in the paragraph element with id="error" (check index.html file)
--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-//creating a promise
-const promise = new Promise ((resolve, reject) => {
-   //this is where you try  to do  the task
-   //this is where we try to turn bread into toast
-   //or water into wine
-   //but there is no guarentee that it will work
-   //so we need a way  to handle either outcome
-   const randomNumber = Math.random() * 100;
-   console.log(randomNumber);
-   if (randomNumber > 50 );
-     } else {  
-        //it was not a success
-        reject(new Error("Invalid number: Too Low"));
-;
-    //Consume the promise
-   promise.then(( resolvedValue)  => {
-     console.log("I knew it! It worked");
-     console.log('It was ${resolvedValue}');
-   }).catch((rejectedValue) => {
-    console.log("Of course it was an error");
-    console.error(rejectedValue);
-   }
-
-   );
- promise
- .then(() => {
-
- }) 
-
-.catch(() => {
-
-});
-
-
+// Creating a promise
+const promise = new Promise((resolve, reject) => {
+    // This is where you try to do the task
+    // This is where we try to turn bread into toast
+    // or water into wine
+    // But there is no guarantee that it will work
+    // so we need a way to handle either outcome
+    const randomNumber = Math.random() * 100;
+    console.log(randomNumber);
+    if (randomNumber > 50) {
+      resolve(randomNumber); // it was a success
+    } else {
+      // it was not a success
+      reject(new Error("Invalid number: Too Low"));
+    }
+  });
+  
+  // Consume the promise
+  promise
+    .then((resolvedValue) => {
+      console.log("I knew it! It worked");
+      console.log(`It was ${resolvedValue}`);
+    })
+    .catch((rejectedValue) => {
+      console.log("Of course, it was an error");
+      console.error(rejectedValue);
+    });
+  
 
 
  
